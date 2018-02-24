cee575b9fdcc1266958c9dceb8e1423934724f8a
Andalon <Andalon@WIN-OB572935SE5>
2018-02-24 14:01:40 +0300

    +РПЗ

--- com.bmstu.notifications.manager/modules/com.bmstu.notifications.manager.core/com.bmstu.notifications.manager.core-service/src/main/resources/META-INF/sql/indexes.sql
+++
@@ -1,3 +0,0 @@
-create index IX_3CF8EF39 on NM_Notification (groupId);
-create index IX_EC567845 on NM_Notification (uuid_[$COLUMN_LENGTH:75$], companyId);
-create unique index IX_B3A4C07 on NM_Notification (uuid_[$COLUMN_LENGTH:75$], groupId);
